1e3a52c3e4b8a0605db1cb76ba01f77ad309ee10
hashamzafar <79524289+hashamzafar@users.noreply.github.com>
2021-04-19 17:59:28 +0200

    c

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-let h1Change = document.getElementsByTagName('h1')
+
+// const h1Function = function(){
+//     let h1Change = document.getElementById('h1')
+//     h1Change[0].innerText = "NoviTel"
+// }
 const h1Function = function(){
-    document.getElementsByTagName(h1).innerHTML = "NoviTel";
+    let oldHeading = document.getElementById('header')  
+    oldHeading.innerText = 'Novi Tel'
 }
+ h1Function()
 
 const backgroundColor = function(){
     document.body.style.background = 'red'
 }
-let myFooter = document.getElementsByClassName("pageFooter")
-
+backgroundColor()
 const changeFooter = function(){
-    onclick.myFooter.innerText = 'this page was loaded on:' + new Date()
+   
+    let myFooter = document.getElementsByClassName("pageFooter")
+
+    myFooter[0].innerHTML = 'this page was loaded on:' + new Date()
 }
+changeFooter()
 
-document.getElementById('a').class.add() += 'link'
+const addCssClass = function(){
+    let newClass = document.querySelectorAll('.link')  
+        for(let i=0; i<newCssClass.length; i++){
+            newClass[i].classList.add("amazon")        
+    }   
+}
+addCssClass()
 
-function myFunction() {
-    var x = document.getElementsByClassName("image-phone");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
+const toggle = function(){
+    let imageToggle = document.querySelectorAll(".Image")
+    for(let i=0; i<imageToggle.length; i++){
+
+        imageToggle[i].style.display = "none"
     }
-  }
-
-let changeColor = document.getElementById('change') 
+toggleClass()
+// let changeColor = document.getElementById('change') 
 
 const changeColor = function () {
     document.body.style.color = "red";
     return false;
 } 
-
-
-
+console.log('hello world')
