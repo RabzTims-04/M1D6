13b82f2a10ce2ba65491af9f97381f1890dbb842
hashamzafar <79524289+hashamzafar@users.noreply.github.com>
2021-04-19 16:00:46 +0200

    Hw M1D6

---
+++ index.js
@@ -0,0 +1,34 @@
+let h1Change = document.getElementsByTagName('h1')
+const h1Function = function(){
+    document.getElementsByTagName(h1).innerHTML = "NoviTel";
+}
+
+const backgroundColor = function(){
+    document.body.style.background = 'red'
+}
+let myFooter = document.getElementsByClassName("pageFooter")
+
+const changeFooter = function(){
+    onclick.myFooter.innerText = 'this page was loaded on:' + new Date()
+}
+
+document.getElementById('a').class.add() += 'link'
+
+function myFunction() {
+    var x = document.getElementsByClassName("image-phone");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+let changeColor = document.getElementById('change') 
+
+const changeColor = function () {
+    document.body.style.color = "red";
+    return false;
+} 
+
+
+
